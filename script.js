9ea0b544eb6e59cb9d80f54d0db7a673276970ae
BUJJA <75597221+Andriwll@users.noreply.github.com>
2023-08-10 02:01:55 -0300

    Keeping it simple

--- script.js
+++
@@ -1,9 +0,0 @@
-function openTab(tabName) {
-    const tabContents = document.getElementsByClassName("tabContent");
-    for (const content of tabContents) {
-      content.style.display = "none";
-    }
-  
-    document.getElementById(tabName + "Content").style.display = "block";
-  }
-  
