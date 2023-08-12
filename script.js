5f0f17b70736f30e79b9bb352700089386418d6e
BUJJA <75597221+Andriwll@users.noreply.github.com>
2023-08-11 22:00:33 -0300

    Initial Commit

--- script.js
+++ script.js
@@ -7,3 +7,38 @@ function mostraResposta(id) {
     var clicada = document.getElementById(id);
     clicada.classList.add('active');
 }
+
+function filterByCategory(category) {
+    var links = document.querySelectorAll('[data-category]');
+    for (var i = 0; i < links.length; i++) {
+        if (category === 'all' || links[i].getAttribute('data-category') === category) {
+            links[i].style.display = 'block';
+        } else {
+            links[i].style.display = 'none';
+        }
+    }
+}
+
+function filterByCategory(category) {
+    var links = document.querySelectorAll('[data-category]');
+    for (var i = 0; i < links.length; i++) {
+        if (category === 'all' || links[i].getAttribute('data-category') === category) {
+            links[i].style.display = 'block';
+        } else {
+            links[i].style.display = 'none';
+        }
+    }
+
+    // Also, hide all content on the right side
+    var respostas = document.getElementsByClassName('faq');
+    for (var i = 0; i < respostas.length; i++) {
+        respostas[i].classList.remove('active');
+    }
+}
+
+function toggleDropdown() {
+    var dropdown = document.getElementById('category-dropdown');
+    dropdown.classList.toggle('active');
+}
+
+
